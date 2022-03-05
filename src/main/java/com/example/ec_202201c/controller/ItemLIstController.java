dfecee60e49a1a87a643dd3ef6f17e0c3735147d
Tetsuya Azami <zqianjian35@gmail.com>
2022-03-05 13:43:08 +0900

    商品一覧機能を修正しました

--- src/main/java/com/example/ec_202201c/controller/ItemLIstController.java
+++ src/main/java/com/example/ec_202201c/controller/ItemLIstController.java
@@ -1,25 +1,37 @@
 package com.example.ec_202201c.controller;
 
 import java.util.List;
-
+import java.util.Locale;
+import com.example.ec_202201c.domain.Item;
+import com.example.ec_202201c.form.ItemSearchForm;
+import com.example.ec_202201c.service.ItemListService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.example.ec_202201c.domain.Item;
-import com.example.ec_202201c.service.ItemListService;
 
 @Controller
 @RequestMapping("/item")
 public class ItemLIstController {
-	/**
-	 * @author Tetsuya Azami
-	 * @return 商品一覧ページ
-	 */
+	@Autowired
+	private MessageSource messageSource;
+
+	@ModelAttribute
+	private ItemSearchForm setUpItemSearchForm() {
+		return new ItemSearchForm();
+	}
+
 	@Autowired
 	private ItemListService itemListService;
 
+	/**
+	 * @author Ono Keitaro
+	 * @return 商品一覧ページ
+	 */
 	@RequestMapping("/list")
 	public String list(Model model) {
 		List<Item> itemList = itemListService.findAll();
@@ -28,17 +40,21 @@ public class ItemLIstController {
 	}
 
 	@RequestMapping("/search")
-	public String searchName(String inputItemName, Model model) {
-		if(inputItemName.equals("")) {
+	public String searchName(@Validated ItemSearchForm itemSearchForm, BindingResult result,
+			Model model) {
+		if (result.hasErrors()) {
 			return list(model);
 		}
-		List<Item> itemList = itemListService.findByLikeName(inputItemName);
-		if(itemList.isEmpty()) {
-			model.addAttribute("noName", "該当する商品がありません");
+		List<Item> itemList = itemListService.findByLikeName(itemSearchForm.getName());
+		if (itemList.isEmpty()) {
+			result.rejectValue("name", "noResultByFuzzySearch");
 			return list(model);
-		}else {
-			model.addAttribute("itemList", itemList);
-			return "item_list_noodle";
 		}
+		model.addAttribute("itemList", itemList);
+
+		// ValidationMessages.propertiesからエラーメッセージ「ResultByFuzzySearch」を取得してviewに渡す(引数にitemListのサイズを渡してます)
+		model.addAttribute("ResultByFuzzySearch", messageSource.getMessage("ResultByFuzzySearch",
+				new String[] {String.valueOf(itemList.size())}, Locale.getDefault()));
+		return "item_list_noodle";
 	}
 }
--- src/main/java/com/example/ec_202201c/service/ItemListService.java
+++ src/main/java/com/example/ec_202201c/service/ItemListService.java
@@ -1,22 +1,21 @@
 package com.example.ec_202201c.service;
 
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.example.ec_202201c.domain.Item;
 import com.example.ec_202201c.repository.ItemListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class ItemListService {
 	@Autowired
 	ItemListRepository itemListRepository;
-	public List<Item> findAll(){
+
+	public List<Item> findAll() {
 		return itemListRepository.findAll();
 	}
-	
-	public List<Item> findByLikeName(String name){
+
+	public List<Item> findByLikeName(String name) {
 		return itemListRepository.findByLikeName(name);
 	}
 }
