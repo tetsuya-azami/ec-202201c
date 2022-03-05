cf3dccff25c16a7ff58a8781f81053bcce773ff2
Tetsuya Azami <zqianjian35@gmail.com>
2022-03-05 10:19:23 +0900

    ユーザ登録機能の修正を行いました

--- src/main/resources/static/js/addressSearch.js
+++ src/main/resources/static/js/addressSearch.js
@@ -1,21 +1,30 @@
-$(document).on("click", "#getAddressBtn", function () {
-  $.ajax({
-    url: "http://zipcoda.net/api",
-    dataType: "jsonp",
-    data: {
-      zipcode: $("#inputZipcode").val(),
-    },
-    async: true,
-  })
-    .done(function (data) {
-      console.log(data);
-      console.dir(JSON.stringify(data));
-      $("#address").val(data.items[0].address);
+(function () {
+  $("#getAddressBtn").on("click", () => {
+    $("#address").val("");
+    $.ajax({
+      url: "http://zipcoda.net/api",
+      dataType: "jsonp",
+      data: {
+        zipcode: $("#zipcode").val(),
+      },
+      async: true,
     })
-    .fail(function (XMLHttpRequest, textStatus, errorThrown) {
-      alert("正しい結果を得られませんでした。");
-      console.log("XMLHttpRequest : " + XMLHttpRequest.status);
-      console.log("textStatus     : " + textStatus);
-      console.log("errorThrown    : " + errorThrown.message);
-    });
-});
+      .done((data) => {
+        $("#address").val(data.items[0].address);
+      })
+      .fail((XMLHttpRequest, textStatus, errorThrown) => {
+        alert("正しい結果を得られませんでした。");
+        console.log("XMLHttpRequest : " + XMLHttpRequest.status);
+        console.log("textStatus     : " + textStatus);
+        console.log("errorThrown    : " + errorThrown.message);
+      });
+
+    // 存在しない郵便番号を入力した場合にzipcodaが結果を返さずdone()の処理にも移行しないため、
+    // やむなくsetTimeout()を使って結果が返ってこなかった場合の処理を記述しています
+    setTimeout(() => {
+      if ($("#address").val() == "") {
+        $("#address").val("存在しない郵便番号です");
+      }
+    }, 800);
+  });
+})();
