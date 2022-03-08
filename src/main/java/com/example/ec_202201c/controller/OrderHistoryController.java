3159a8076f7aa9c0227439548c51bcf8e558a782
ono <keitaro.ono@rakus-partners.co.jp>
2022-03-08 15:52:39 +0900

    コンフリクト解消

--- src/main/java/com/example/ec_202201c/controller/OrderHistoryController.java
+++ src/main/java/com/example/ec_202201c/controller/OrderHistoryController.java
@@ -1,5 +1,6 @@
 package com.example.ec_202201c.controller;
 
+<<<<<<< HEAD
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +10,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+=======
+>>>>>>> develop
 import com.example.ec_202201c.domain.Account;
 import com.example.ec_202201c.domain.Order;
 import com.example.ec_202201c.service.OrderHistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/order")
